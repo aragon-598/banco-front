205b529841320667640b7ddd93e7986a91f4516f
Alejandro Aragon <aragondru155@gmail.com>
2022-01-13 16:27:46 -0600

    Frontend para prueba consiti

---
+++ src/components/Navbar.vue
@@ -0,0 +1,46 @@
+<template>
+  <v-card
+    color="grey lighten-4"
+    flat
+    tile
+  >
+    <v-toolbar extended
+        dark
+    >
+      <v-app-bar-nav-icon class="white--text"></v-app-bar-nav-icon>
+
+      <v-toolbar-title>Title</v-toolbar-title>
+
+      <v-spacer></v-spacer>
+
+      <v-btn 
+        icon
+        class="white--text"
+      >
+        <v-icon>mdi-magnify</v-icon>
+      </v-btn>
+
+      <v-btn 
+        icon
+        class="white--text"
+      >
+        <v-icon>mdi-heart</v-icon>
+      </v-btn>
+
+      <v-btn 
+        icon
+        class="white--text"
+      >
+        <v-icon>mdi-dots-vertical</v-icon>
+      </v-btn>
+    </v-toolbar>
+  </v-card>
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style>
+    
+</style>
---
+++ src/components/home/Clientes.vue
@@ -0,0 +1,95 @@
+<template>
+  <v-simple-table>
+    <template v-slot:default>
+      <thead>
+        <tr>
+          <th class="text-left">
+            Name
+          </th>
+          <th class="text-left">
+            Calories
+          </th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr
+          v-for="item in desserts"
+          :key="item.name"
+        >
+          <td>{{ item.name }}</td>
+          <td>{{ item.calories }}</td>
+        </tr>
+      </tbody>
+    </template>
+  </v-simple-table>
+</template>
+<script>
+
+import axios from 'axios'
+
+  export default {
+    data () {
+      return {
+        desserts: [
+          {
+            name: 'Frozen Yogurt',
+            calories: 159,
+          },
+          {
+            name: 'Ice cream sandwich',
+            calories: 237,
+          },
+          {
+            name: 'Eclair',
+            calories: 262,
+          },
+          {
+            name: 'Cupcake',
+            calories: 305,
+          },
+          {
+            name: 'Gingerbread',
+            calories: 356,
+          },
+          {
+            name: 'Jelly bean',
+            calories: 375,
+          },
+          {
+            name: 'Lollipop',
+            calories: 392,
+          },
+          {
+            name: 'Honeycomb',
+            calories: 408,
+          },
+          {
+            name: 'Donut',
+            calories: 452,
+          },
+          {
+            name: 'KitKat',
+            calories: 518,
+          },
+        ],
+        clientes:[]
+      }
+    },
+    methods:{
+        getAllClientes(){
+            axios.get('http://localhost:5000/cliente/allclients').
+            then(response => {
+                console.log(response);
+                this.clientes = response;
+            }).
+            catch((e)=>console.log(e))
+        }
+    },
+    mounted() {
+        this.getAllClientes();
+    },
+  }
+</script>
+<style lang="">
+    
+</style>
